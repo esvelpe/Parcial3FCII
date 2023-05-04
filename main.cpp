097bc8461d6d46bc47950996fc6608e454192529
josuejaramillo <josue.jaramillo@udea.edu.co>
2023-05-04 16:51:13 -0500

    Clase

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <iomanip>
 #include <math.h>
-#include "LinearShooting.cpp"
+//#include "LinearShooting.cpp"
+#include "LinearShootingClass.h"
 using namespace std;
 
 double p(double x)
@@ -43,10 +44,12 @@ double r(double x)
 int main()
 {
     double (*f[3])(double) = {p, q, r};
-    double w[11][2];
-
-    LinearShooting(1.0, 2.0, 1.0, 2.0, 10, f, w);
-
+    //double w[11][2];
+    //LinearShooting(1.0, 2.0, 1.0, 2.0, 10, f, w);
+    
+    LinearShooting EDOb(1.0, 2.0, 1.0, 2.0, 10, f);
+    double **w = EDOb.solutions();
+    
     cout << "# x" << setw(8) << "y" << endl;
     for (int i = 0; i < 11; i++)
     {
